7f3286d7cbc66ff5b60304384b3cd9b0657a456d
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-18 21:51:29 +0100

    Add recipeFoods controller

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,29 @@
+class RecipeFoodsController < ApplicationController
+  def def new
+    @recipe_food = RecipeFood.new
+  end
+
+  def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    if recipe_food.save
+      redirect_to recipe_path(id: params[:recipe_id]) 
+      flash[:success] = 'Ingredient added successfuly'
+    else 
+      redirect_to new_recipe_recipe_food_path
+      flash[:alert] = 'Try again'
+    end
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy
+    redirect_to recipe_path(Recipe.find(params[:recipe_id])
+  end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity)
+  end
+
+end
