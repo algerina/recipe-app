9aacea4fb61655f058794fee1e53e0b8b12f57de
Bondok6 <kirolousbondok6@gmail.com>
2022-05-20 02:39:50 +0200

    Fix Logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'foods#index'
-  devise_for :users 
+  devise_for :users
+
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   
   resources :foods
   resources :recipes do
